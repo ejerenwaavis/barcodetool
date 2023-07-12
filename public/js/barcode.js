26ad3e58f32f25938ef1b514aff370652eb46371
Avis Ejerenwa <ejerenwaavis@gmail.com>
2023-07-12 02:43:18 -0400

    stable print and save

--- public/js/barcode.js
+++ public/js/barcode.js
@@ -6,23 +6,25 @@ window.onload = (event) => {
   
 };
 
-function render(evt){
-  field = $(evt);
+function render(){
+  field = $("#barcodeNumber");
   text = (field.val()).toUpperCase();
   if(text){
     $("#printButton").removeClass("disabled");
     JsBarcode("#barcode", text, {
       // lineColor: "#7777",
       width:(text.length > 20)? 1.2 : 1.7,
-      height:80,
+      marginTop: 50,
+      height:100,
       displayValue: true
     });
+
   }else{
     $("#printButton").addClass("disabled");
     JsBarcode("#barcode", " ", {
       lineColor: "#9999",
       // width:4,
-      height:80,
+      height:85,
       displayValue: false
     });
   }
@@ -36,7 +38,8 @@ function transferDescription(evt){
 
 function clearNameField() {
   $("#barcodeDescription").val("");
-   $("#description").val("")
+   $("#description").val("");
+   render();
 }
 
 function printBarcode() {
@@ -44,10 +47,30 @@ function printBarcode() {
     $("#description").val(" ");
   }
   window.print();
-  // body.html(originalContents);
-
-
 }
+
+function downloadImage() {
+  var name = " ";
+  if($("#description").val().length > 0){
+    name = ($("#description").val()).toUpperCase();;
+  }
+  
+  var origanlCanvas = $("#barcode")[0];
+  var canvas = $("#barcode")[0];
+  const ctx = canvas.getContext("2d");
+
+
+  
+  ctx.font = "20px Arial";
+  ctx.textAlign = "center";
+  ctx.fillText(name ,-110,35);
+  console.log(ctx);
+  console.log(ctx.width);
+
+  var dt = canvas.toDataURL('image/jpeg');
+  $("#downloadLink").attr("download", name) ;
+  $("#downloadLink").attr("href", dt) ;
+};
 
 function search(evt) {
   tracking = ($(evt).val()).toUpperCase();
